11d15e5cd092f6e33c7921091a8002db28038617
arunsachan <66840949+arunsachan@users.noreply.github.com>
2023-01-26 18:16:34 +0530

    'done'

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+let inpi=document.getElementById('input');
+
+let expresso="";
+function solve(e){
+    if(e == 'ans'){
+        // Eval below is the inbuilt function for solving the expressions. 
+        let result=eval(expresso);
+        console.log(result);
+         inpi.value=result;
+    }
+    else if(e == 'clear'){
+        expresso ="";
+        console.log(expresso);
+        inpi.value=expresso;
+    }
+
+    else{
+        expresso += e;
+    console.log(expresso);
+     inpi.value=expresso;
+    }
+}
